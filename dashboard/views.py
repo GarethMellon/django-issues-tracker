f6990d87e6fd419d20dd3f8707fba89ef8b7e9ec
Gareth_Mellon <garethmellon@gmail.com>
2019-03-14 19:36:46 +0000

    added tickets to dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from tickets.models import Ticket
 
 # Create your views here.
 
 def dashboard_page(request):
-    return render(request, 'dashboard.html')
+    tickets = Ticket.objects.all()
+    return render(request, 'dashboard.html', {'tickets': tickets})
