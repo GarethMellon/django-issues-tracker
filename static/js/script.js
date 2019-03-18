8f0318069431215fa3d6ce48ce7867ceb5a7099a
Gareth_Mellon <garethmellon@gmail.com>
2019-03-18 18:54:31 +0000

    Added some jQuery to show/hide the pay button if feature is seleted

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,24 @@
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
-})
+$(document).ready(function() {
+    console.log("ready!");
+
+    $('.stripe-button-el').hide()
+
+    $('#myModal').on('shown.bs.modal', function() {
+        $('#myInput').focus()
+    })
+
+    $('#id_ticket_type').click(function() {
+        if ($(this).val() === "Feature") {
+            $(this).siblings('.btn-default').hide()
+            $(this).siblings('.stripe-button-el').show()
+        }
+        else if ($(this).val() === "Bug") {
+            $(this).siblings('.btn-default').show()
+            $(this).siblings('.stripe-button-el').hide()
+        }
+        else {
+            $(this).siblings('.btn-default').show()
+            $(this).siblings('.stripe-button-el').hide()
+        };
+    })
+});
